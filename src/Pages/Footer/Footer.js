50398118ff45046a61ce2dc3f706303c5d09ff6b
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-11-08 23:15:20 +0600

    footer added

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <p><small>Copyright coming soon-2022</small></p>
+        <div className='text-center bg-dark text-light p-5'>
+            <p>
+                <small>Copyright coming soon-2022</small></p>
+            <h6> S M Mustakim Bellah</h6>
+
+            <h2 className='m-2'><FaFacebook></FaFacebook><FaLinkedin></FaLinkedin><FaGithub></FaGithub></h2>
+
         </div>
     );
 };
